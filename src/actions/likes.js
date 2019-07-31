a12aaeec7a8fbf7523718d5bf3f66f3f42717b9e
Tommy Rosario <tommy@tommyrosario.com>
2019-07-31 18:29:21 -0400

    handling for max number of likes

--- src/actions/likes.js
+++ src/actions/likes.js
@@ -4,12 +4,25 @@ import {
   USER_ERROR
 } from "../actionTypes";
 
+import { LIKES } from '../config/index';
+
 export function addLike(like) {
 	return (dispatch, getState) => {
 		const { likes, giphy } = getState();
 
 		// check for dups
 		const found = likes.collection.some(item => item.id === like.id);
+
+		// check if min number of likes met
+		const maxed = likes.collection.length === LIKES.min;
+
+		if (maxed) {
+			dispatch({
+				type: USER_ERROR,
+				msg: 'You reached your max number of likes.'
+			});			
+			return false;
+		}
 		
 		if (found) {
 			dispatch({
