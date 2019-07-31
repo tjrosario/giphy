1d1ea5dabbe8238f9bc6f151f19b90d2abacf406
Tommy Rosario <tommy@tommyrosario.com>
2019-07-31 01:01:42 -0400

    action types for giphy search and likes

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -1,2 +1,8 @@
+// giphy
+export const REQUEST_GIPHY = "REQUEST_GIPHY";
+export const RECEIVE_GIPHY = "RECEIVE_GIPHY";
+export const UPDATE_WEIRDNESS = "UPDATE_WEIRDNESS";
+
+// likes
 export const ADD_LIKE = "ADD_LIKE";
 export const REMOVE_LIKE = "REMOVE_LIKE";
