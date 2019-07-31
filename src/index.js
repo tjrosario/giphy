78580217668e7f95795e76c0d57dcf0da4695ccb
Tommy Rosario <tommy@tommyrosario.com>
2019-07-31 01:03:18 -0400

    adding redux-thunk and cross-fetch

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 
 import App from './App';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));	
 
 class Root extends React.Component {
   render() {
