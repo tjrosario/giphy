cc3ac530be0b5325b24dd6ac23a331e330273218
Tommy Rosario <tommy@tommyrosario.com>
2019-07-31 20:10:42 -0400

    prevent liking search term more than once

--- src/actions/likes.js
+++ src/actions/likes.js
@@ -10,11 +10,22 @@ export function addLike(like) {
 	return (dispatch, getState) => {
 		const { likes, giphy } = getState();
 
+		// check if term already liked
+		const termExists = likes.collection.some(item => item.term.toLowerCase() === giphy.term.toLowerCase());
+
 		// check for dups
 		const found = likes.collection.some(item => item.id === like.id);
 
 		// check if min number of likes met
 		const maxed = likes.collection.length === LIKES.min;
+
+		if (termExists) {
+			dispatch({
+				type: USER_ERROR,
+				msg: 'You already liked this search term.  Try searching again!'
+			});			
+			return false;
+		}
 
 		if (maxed) {
 			dispatch({
